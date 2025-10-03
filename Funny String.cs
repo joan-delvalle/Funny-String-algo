3f2636251041de8aea1300612f1ae65a7de488a4
joan-delvalle <joandelvalle865@gmail.com>
2025-10-03 08:32:15 -0700

    Funny String.cs

---
+++ Funny String.cs
@@ -0,0 +1,65 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string funnyString(string s)
+    {
+        int n = s.Length;
+
+    for (int i = 1; i < n; i++)
+    {
+        int diffOriginal = Math.Abs(s[i] - s[i - 1]);
+        int diffReversed = Math.Abs(s[n - i] - s[n - i - 1]);
+
+        if (diffOriginal != diffReversed)
+            return "Not Funny";
+    }
+
+    return "Funny";
+
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = Result.funnyString(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
